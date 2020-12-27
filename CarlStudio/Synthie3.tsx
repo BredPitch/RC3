c5095a5ceb7a4c313d5a0d1d65d1bf2be0d15407
BredPitch <76606141+BredPitch@users.noreply.github.com>
2020-12-27 18:07:46 +0100

    Add files via upload

---
+++ CarlStudio/Synthie3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Synthie3" tilewidth="32" tileheight="32" tilecount="190" columns="19">
+ <image source="../../Synthie3.PNG" width="609" height="339"/>
+</tileset>
